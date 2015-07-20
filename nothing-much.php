972c95cb30ef8c9853d022a326e10d225380f37f
ocean90 <ocean90@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-20 21:35:43 +0000

    Test for Plugins Directory SVN Tracker and i18n.
    git-svn-id: https://plugins.svn.wordpress.org/nothing-much/tags/1.3@1202638 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nothing-much.php
+++ nothing-much.php
@@ -21,7 +21,7 @@ class NothingMuch {
     }
 
     public function admin_notices() {
-        echo '<div class="updated">' . __( 'We told you this plugin didn\'t do much. :)', 'nothing-much' ) . '</div>';
+        echo '<div class="updated">' . __( 'We told you this plugin didn\'t do much. :(', 'nothing-much' ) . '</div>';
     }
 }
 
