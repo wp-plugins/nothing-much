402076c6e8555d964224b31b0b19234ac87f0d71
ocean90 <ocean90@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-20 20:38:22 +0000

    Add some translation files.
    git-svn-id: https://plugins.svn.wordpress.org/nothing-much/trunk@1202604 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- nothing-much.php
+++ nothing-much.php
@@ -1,17 +1,23 @@
 <?php
 /*
 Plugin Name: Nothing Much
-Plugin URI: http://wordpress.org/plugins/nothing-much/
+Plugin URI: http://wordpress.org/plugins/nothing/
 Description: This plugin does nothing of value, and is used to test things in the plugin directory/repo.
 Author: stephdau
 Version: 1.3
 Text Domain: nothing-much
+Domain Path: /languages
 License: GPL version 2 or later - http://www.gnu.org/licenses/old-licenses/gpl-2.0.html
 */
 
 class NothingMuch {
     public function __construct() {
+        add_action( 'admin_init', array( $this, 'load_textdomain' ) );
         add_action( 'admin_notices', array( $this, 'admin_notices' ) );
+    }
+
+    public function load_textdomain() {
+        load_plugin_textdomain( 'nothing-much', false, dirname( plugin_basename( __FILE__ ) ) . '/languages' );
     }
 
     public function admin_notices() {
